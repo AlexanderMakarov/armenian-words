9916ae51fba9dbdfa3c0153b5564b780a8b009ac
Alexander Makarov <2630978+AlexanderMakarov@users.noreply.github.com>
2026-01-25 13:40:58 +0400

    Change vocabulary source, add audio links and etymology for future needs (#12)
    * Few more updates in vocabulary.
    
    * Added "scripts/build_vocabulary_v2.py" to use new source, add audio links and etymology.
    
    * Fixed vocabulary search performance. FIxed tons of linter issues.

--- src/routes/quiz/+page.svelte
+++ src/routes/quiz/+page.svelte
@@ -4,7 +4,6 @@ import { get } from 'svelte/store';
 import { goto } from '$app/navigation';
 import { base } from '$app/paths';
 import { trackQuizComplete } from '$lib/analytics.js';
-import { ProgressBar, QuizOption } from '$lib/components/index.js';
 import { QUIZ_CONFIG } from '$lib/constants.js';
 import {
     cardsCount,
